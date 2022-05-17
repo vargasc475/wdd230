b8569727127217eea1d0a37570b75d495a36e791
Christian <vargasc475@gmail.com>
2022-05-17 17:13:23 -0400

    Added website to make a list of favorites BoM chapters

---
+++ bomActivityW5/js/bom.js
@@ -0,0 +1,22 @@
+const input = document.getElementById('favchap');
+const button = document.getElementById('button'); 
+const list = document.querySelector('.list');
+
+button.addEventListener('click', function() {
+    const li = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    
+    li.textContent = input.value;
+    deleteBtn.textContent = `❌`;
+    
+    li.appendChild(deleteBtn);
+
+    list.appendChild(li);
+    
+    deleteBtn.addEventListener('click', function() {list.removeChild(li);})
+
+    input.focus()
+
+    input.value = '';
+
+})
