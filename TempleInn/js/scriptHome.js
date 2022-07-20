330f01663309218d3698a1dbca273c6ca0d67ead
Christian <vargasc475@gmail.com>
2022-07-20 12:21:53 -0400

    FIxed the script file error

--- TempleInn/js/scriptHome.js
+++ TempleInn/js/scriptHome.js
@@ -37,34 +37,34 @@ lastM.appendChild(last);
 
 lastM2.appendChild(completeSentence);
 
-// // DISPLAY TEMPLE QUOTES
+// DISPLAY TEMPLE QUOTES
 
-// const quoteUrl = 'data/templeQuotes.json';
+const quoteUrl = 'data/templeQuotes.json';
 
-// async function quoteFetch() {
-//     let response = await fetch(quoteUrl);
-//     if (response.ok) {
-//         let data = await response.json()
-//         displayQuote(data);
-//     } else {
-//         throw Error(response.statusText0)
-//     }
-// }
+async function quoteFetch() {
+    let response = await fetch(quoteUrl);
+    if (response.ok) {
+        let data = await response.json()
+        displayQuote(data);
+    } else {
+        throw Error(response.statusText0)
+    }
+}
 
-// quoteFetch()
+quoteFetch()
 
-// let randomNumber = Math.floor(Math.random() * 5);
+let randomNumber = Math.floor(Math.random() * 5);
 
-// // get element and create elements
-// const quoteDiv = document.querySelector('.quote');
-// const h1 = document.createElement('h1');
-// const p = document.createElement('p');
+// get element and create elements
+const quoteDiv = document.querySelector('.quote');
+const h1 = document.createElement('h1');
+const p = document.createElement('p');
 
-// function displayQuote(data) {
-//     h1.innerHTML = `"${data.quotes[randomNumber].quote}"`;
-//     p.innerHTML = `<strong>${data.quotes[randomNumber].name}</strong>`;
+function displayQuote(data) {
+    h1.innerHTML = `"${data.quotes[randomNumber].quote}"`;
+    p.innerHTML = `<strong>${data.quotes[randomNumber].name}</strong>`;
 
-//     quoteDiv.append(h1);
-//     quoteDiv.append(p);
+    quoteDiv.append(h1);
+    quoteDiv.append(p);
 
-// }
+}
